5d75fa769084a01547dbeb1541dccd95b347a734
sethvincent <sethvincent@gmail.com>
2012-12-03 16:43:18 -0800

    add heart icons

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
-  var width = $(window).width();
-  console.log(width);
-  $(window).resize(function() {
-    var new_width = $(window).width();
-    
-    console.log(new_width);
-  })
+
 });
